736ebeb4439aafe4a3092d24ed7ebb65d8493b3f
Devco2011 <devco2011@gmail.com>
2020-09-01 14:59:07 -0500

    finished Chapter 4

---
+++ scripts/messages/MessageList.js
@@ -0,0 +1,31 @@
+import { getMessagesByFriend } from "./MessageProvider.js"
+import { Message } from "./Message.js"
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".messages")
+const friendListSection = document.querySelector(".friends")
+
+eventHub.addEventListener("friendSelected", event => {
+    const friendName = event.detail.friend
+    const messages = getMessagesByFriend(friendName)
+    render(messages)
+})
+/*
+    COMPONENT FUNCTION
+*/
+export const MessageList = () => {
+    render([])
+}
+
+/*
+    RENDERING FUNCTION
+*/
+const render = messageArray => {
+    const convertedMessages = messageArray.map(messageObject => {
+        const messageHTML = Message(messageObject)
+        return messageHTML
+    })
+    const combinedSections = convertedMessages.join("")
+    contentTarget.innerHTML = combinedSections
+}
+
+
